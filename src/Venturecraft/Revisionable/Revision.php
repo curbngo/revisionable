5c10f604c5a3e92d5ad67911fd4a4a9a2eb18d57
monove <overlander@gmail.com>
2021-05-12 14:26:13 -0400

    Update Revision.php

--- src/Venturecraft/Revisionable/Revision.php
+++ src/Venturecraft/Revisionable/Revision.php
@@ -290,4 +290,15 @@ class Revision extends Eloquent
             return $value;
         }
     }
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
